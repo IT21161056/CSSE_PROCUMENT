848d0794a8d4c295fce2c3db00af0dd65a749645
IT21161810 <Mvirajselling@gmail.com>
2023-10-13 00:06:29 +0530

    order summary added

--- frontend-mobile/screen/PlaceOrder.js
+++ frontend-mobile/screen/PlaceOrder.js
@@ -11,48 +11,20 @@ export default function PlaceOrder() {
   const navigation = useNavigation();
 
   const [value, setValue] = useState(null);
-  const [isFocus, setIsFocus] = useState(false);
   const [total, setTotal] = useState(0);
-  const [productlist,setProductList] = useState([])
+  const [productlist, setProductList] = useState([])
   const [sitelist, setSites] = useState([])
-
-  const [siteInfo, setSiteInfo] = useState({ key: value, name: '' });
-  
-
-  const siteNames = sitelist.map((site) => ({
-    key: site._id,  // Displayed in the dropdown
-    value: site.siteName, 
-  }));
-  const productNames = productlist.map((product) => (product.productName))
-
-  const fetchSiteList = async () => {
-    try {
-      const res = await axios.get("http://192.168.43.95:8072/site/")
-      const data = res.data;
-      return (data)
-    } catch (error) {
-      console.error("Error fetching site list:", error);
-      throw error;
-    }
-  };
-  
-  const fetchProductList = async () => {
-    try {
-      const res = await axios.get("http://192.168.43.95:8072/product/")
-      const data = res.data;
-      return (data)
-    } catch (error) {
-      console.error("Error fetching product list:", error);
-      throw error;
-    }
-  };
-
-  useEffect(() => {
-    fetchSiteList().then((data) => setSites(data));
-    fetchProductList().then((data) => setProductList(data));
-  }, [])
-
+  const [supplierlist, setSupplierList] = useState([])
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [siteInfo, setSiteInfo] = useState({ key: value, name: '' });
+  const [order, setOrder] = useState([])
+  const [site, setSite] = useState();
+  const [selectedDate, setSelectedDate] = useState('required Date')
+  const [supplier, setSupplier] = useState();
+  const [product, setProduct] = useState();
+  const [qnty, setQnty] = useState();
+  const [price, setPrice] = useState(0)
+  const [prods, setProds] = useState([])
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -62,6 +34,49 @@ export default function PlaceOrder() {
     setDatePickerVisibility(false);
   };
 
+  const siteNames = sitelist.map((site) => ({
+    key: site._id,
+    value: site.siteName,
+  }));
+
+  const productNames = productlist.map((product) => (product.productName))
+
+  const supList = supplierlist.flatMap((supplier) => {
+    return supplier.productList.map((product) => ({
+      value: `${supplier.name} Rs. ${product.price}.00`,
+    }));
+  });
+
+
+  const fetchSiteList = async () => {
+    try {
+      const res = await axios.get("http://192.168.8.166:8072/site/")
+      const data = res.data;
+      return (data)
+    } catch (error) {
+      console.error("Error fetching site list:", error);
+      throw error;
+    }
+  };
+
+  const fetchProductList = async () => {
+    try {
+      const res = await axios.get("http://192.168.8.166:8072/product/")
+      const data = res.data;
+      return (data)
+    } catch (error) {
+      console.error("Error fetching product list:", error);
+      throw error;
+    }
+  };
+
+
+  useEffect(() => {
+    fetchSiteList().then((data) => setSites(data));
+    fetchProductList().then((data) => setProductList(data));
+  }, [])
+
+
   const handleConfirm = (date) => {
     const dt = new Date(date)
     const rdate = dt.toISOString().split("T");
@@ -70,24 +85,11 @@ export default function PlaceOrder() {
     hideDatePicker();
   };
 
-  const [order, setOrder] = useState([])
-
-  const [site, setSite] = useState();
-  const [selectedDate, setSelectedDate] = useState('required Date')
-  const [supplier, setSupplier] = useState();
-  const [product, setProduct] = useState();
-  const [qnty, setQnty] = useState();
-  const [price, setPrice] = useState(0)
-  const [prods, setProds] = useState([])
-
-  console.log(prods)
   const handleOrder = () => {
-
     const calculatedTotal = calculateTotal(prods);
-
     const newOrder = {
       site: siteInfo.value,
-      siteId : siteInfo.key,
+      siteId: siteInfo.key,
       selectedDate,
       prods,
       total: calculatedTotal
@@ -97,32 +99,52 @@ export default function PlaceOrder() {
     setSupplier("");
     setProduct("");
     setQnty("");
-    // navigation.navigate('Place2', { order })
+    navigation.navigate('Place2', { order })
 
   };
 
-  //function to add repeatedly supplier product and quantity
   const handleProducts = () => {
     if (supplier && product && qnty) {
-      // Find the selected product and its price
-      
-      // Find the selected supplier and its price
-      const selectedSupplier = suppliers[product].find(item => item.key === supplier);
-
-      if (selectedSupplier) {
-        const newItem = {
-          product: product,
-          supplier: selectedSupplier.value,
-          qnty,
-          price: selectedSupplier.price  // Set the product price
-        };
-        setProds([...prods, newItem]);
-        Alert.alert('Product added successfully');
-        setSupplier(null);
-        setQnty('');
-      }
+      const newItem = {
+        product: product,
+        supplier: supplier,
+        qnty,
+        price
+      };
+      setProds([...prods, newItem]);
+      Alert.alert('Product added successfully');
+      setSupplier(null);
+      setQnty('');
     }
   };
+
+
+  const getSupplierByProduct = async (itemName) => {
+    setProduct(itemName)
+    try {
+      const res = await axios.get(`http://192.168.8.166:8072/supplier/byItem?itemName=${itemName}`)
+      setSupplierList(res.data)
+
+    } catch (error) {
+      console.error("Error fetching product list:", error);
+      throw error;
+    }
+  }
+
+  const handleSelect = (val) => {
+    const selectedProduct = supList.find((product) => product.value === val);
+    if (selectedProduct) {
+      // Extract the supplier name from the selected product's value
+      const supplierName = selectedProduct.value.split(' Rs. ')[0];
+      // Extract the price from the selected product's value
+      const priceMatch = selectedProduct.value.match(/Rs\. (\d+\.\d+)/);
+      const selectedPrice = priceMatch ? parseFloat(priceMatch[1]) : 0;
+      // Update the selected supplier (only the name) and price
+      setSupplier(supplierName);
+      setPrice(selectedPrice);
+    }
+  };
+
 
   const calculateTotal = (prods) => {
     let total = 0;
@@ -132,54 +154,13 @@ export default function PlaceOrder() {
     return total;
   };
 
-  // const products = [
-  //   { key: '1', value: 'metal' },
-  //   { key: '2', value: 'sand' },
-  //   { key: '3', value: 'cement' },
-  //   { key: '4', value: 'paint' },
-  //   { key: '5', value: 'bricks' },
-  //   { key: '6', value: 'iron bars' },
-  // ]
-
-  const suppliers = {
-    '1': [
-      { key: '7', value: 'kamal hardware', price: 380 },
-      { key: '8', value: 'sachin hardware', price: 450 },
-    ],
-    '2': [
-      { key: '9', value: 'pasindu hardware', price: 1500 },
-      { key: '10', value: 'nimal hardware', price: 500 },
-    ],
-    '3': [
-      { key: '11', value: 'saman hardware', price: 420 },
-      { key: '12', value: 'viraj hardware', price: 310 },
-    ],
-    '4': [
-      { key: '13', value: 'ganidu hardware', price: 560 },
-      { key: '14', value: 'ravin hardware', price: 680 },
-    ],
-    '5': [
-      { key: '15', value: 'namal hardware', price: 570 },
-      { key: '16', value: 'anoj hardware', price: 630 },
-    ],
-    '6': [
-      { key: '17', value: 'nirmal hardware', price: 780 },
-      { key: '18', value: 'dasun hardware', price: 670 },
-    ]
-  }
-
-  const selectedSuppliers = suppliers[product] || [];
-  const suppliersData = selectedSuppliers.map(item => ({
-    key: item.key,
-    value: `${item.value} - Rs. ${item.price}`
-  }));
 
   return (
     <SafeAreaView>
       <Text style={styles.place}>Place Order</Text>
 
-      <View style={{ marginTop: 50, justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 18, fontWeight: "600", marginRight: 20, marginLeft: -240, top: -13 }}>Select Site</Text>
+      <View style={styles.selectSite}>
+        <Text style={styles.selectText}>Select Site</Text>
         <SelectList
           setSelected={(val) => {
             const selectedSite = sitelist.find(item => item._id === val);
@@ -188,13 +169,13 @@ export default function PlaceOrder() {
           }}
           data={siteNames}
           placeholder="Select supplier"
-          boxStyles={{ borderColor: '#ffa366', height: 50, width: 350, top: -10 }}
+          boxStyles={styles.siteBox}
         />
       </View>
 
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.rdate}>
         <TouchableOpacity onPress={() => { showDatePicker() }}>
-          <Text style={{ fontSize: 18, fontWeight: "600", marginLeft: 15 }}>Required date</Text>
+          <Text style={styles.rdatetxt}>Required date</Text>
           <DateTimePickerModal
             isVisible={isDatePickerVisible}
             mode="date"
@@ -205,39 +186,37 @@ export default function PlaceOrder() {
         </TouchableOpacity>
       </View>
 
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 18, fontWeight: "600", marginLeft: -230, bottom: 8 }}>Select product</Text>
+      <View style={styles.productselect}>
+        <Text style={styles.productText}>Select product</Text>
         <SelectList
-          setSelected={(val) => setProduct(val)}
+          setSelected={(val) => getSupplierByProduct(val)}
           data={productNames}
           placeholder="Select product"
-          boxStyles={{ borderColor: '#ffa366', height: 50, width: 350 }}
+          boxStyles={styles.productBox}
         />
       </View>
 
-      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-        <Text style={{ fontSize: 18, fontWeight: "600", bottom: 3, marginRight: -45, marginTop: 13, marginLeft: -280 }}>Select supplier</Text>
+      <View style={styles.suppplierView}>
+        <Text style={styles.supplierText}>Select supplier</Text>
         <SelectList
-          setSelected={(val) => setSupplier(val)}
-          data={suppliersData}
+          setSelected={(val) => handleSelect(val)}
+          data={supList}
           placeholder="Select supplier"
-          boxStyles={{ borderColor: '#ffa366', marginTop: 5, height: 50, width: 350 }}
+          boxStyles={styles.supplierBox}
         />
       </View>
 
-      <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 3 }}>
-        <Text style={{ fontSize: 18, fontWeight: "600", marginLeft: -280 }}>Quantity</Text>
+      <View style={styles.qnty}>
+        <Text style={styles.qntyText}>Quantity</Text>
         <TextInput style={styles.input} placeholder="Quantity" onChangeText={(text) => setQnty(text)} />
       </View>
 
-      <View style={{ marginLeft: 180 }}>
-
-        <TouchableOpacity onPress={handleProducts} style={{ backgroundColor: '#ffe6cc', height: 50, width: 50, borderRadius: 50, justifyContent: 'center' }}>
-          <Icon name="add" style={{ marginLeft: 10, color: '#00004d' }} size={32} color="#4F8EF7" />
+      <View style={styles.ad}>
+        <TouchableOpacity onPress={handleProducts} style={styles.ad1}>
+          <Icon name="add" style={styles.ad2} size={32} color="#4F8EF7" />
         </TouchableOpacity>
-        <Text style={{ marginLeft: 60, bottom: 34, color: 'gray' }}>Add more products</Text>
+        <Text style={styles.ad3}>Add more products</Text>
       </View>
-
 
       <View style={styles.rds}>
         <Button onPress={handleOrder} style={styles.nxt} title="Next" color='#ffa366' />
@@ -257,10 +236,39 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderColor: '#ffa366',
   },
+  ad: {
+    marginLeft: 180
+  },
   place: {
     marginLeft: 130,
     fontSize: 27,
     top: 20
+  },
+  ad1: {
+    backgroundColor: '#ffe6cc',
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    justifyContent: 'center'
+  },
+  ad2: {
+    marginLeft: 10,
+    color: '#00004d'
+  },
+  ad3: {
+    marginLeft: 60,
+    bottom: 34,
+    color: 'gray'
+  },
+  productBox: {
+    borderColor: '#ffa366',
+    height: 50,
+    width: 350
+  },
+  qnty: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 3
   },
   rds: {
     marginLeft: 80,
@@ -269,9 +277,76 @@ const styles = StyleSheet.create({
     color: '#ffa366',
     gap: 5,
   },
+  addmore: {
+    backgroundColor: '#ffe6cc',
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    justifyContent: 'center'
+  },
+  qntyText: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginLeft: -280
+  },
+  suppplierView: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  supplierText: {
+    fontSize: 18,
+    fontWeight: "600",
+    bottom: 3,
+    marginRight: -45,
+    marginTop: 13,
+    marginLeft: -280
+  },
+  selectSite: {
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  supplierBox: {
+    borderColor: '#ffa366',
+    marginTop: 5,
+    height: 50,
+    width: 350
+  },
+  selectText: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginRight: 20,
+    marginLeft: -240,
+    top: -13
+  },
   container1: {
     flex: 1,
     padding: 20,
+  },
+  rdate: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  rdatetxt: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginLeft: 15
+  },
+  productselect: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  productText: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginLeft: -230,
+    bottom: 8
+  },
+  siteBox: {
+    borderColor: '#ffa366',
+    height: 50,
+    width: 350,
+    top: -10
   },
   selectContainer: {
     marginBottom: 20,
