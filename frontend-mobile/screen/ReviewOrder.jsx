66cfda50d8c04fcab0d097c9dce2237efcbc93c5
IT21161056 <99235396+IT21161056@users.noreply.github.com>
2023-10-13 19:54:06 +0530

    review screen

--- frontend-mobile/screen/ReviewOrder.jsx
+++ frontend-mobile/screen/ReviewOrder.jsx
@@ -1,5 +1,5 @@
 import { useRoute } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -8,68 +8,93 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { RadioButton, TextInput } from "react-native-paper";
+import {
+  Button,
+  PaperProvider,
+  Portal,
+  Modal,
+  TextInput,
+} from "react-native-paper";
+import TableRow from "../components/TableRow";
 
 const ReviewOrder = ({ route }) => {
   const { params } = useRoute();
+  const [visible, setVisible] = React.useState(false);
+  const [review, setReview] = useState("");
+
   const itemData = route.params.order;
-  const [value, setValue] = React.useState();
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.tableColumns}>
-        <Text style={{ ...styles.thead }}>Name</Text>
-        <Text style={{ ...styles.thead }}>Supplier</Text>
-        <Text style={{ ...styles.thead }}>Quantity</Text>
-        <Text style={{ ...styles.thead }}>Complete status</Text>
-      </View>
-      <ScrollView scrollEnabled={true} style={{ height: 300 }}>
-        {itemData.orderList.map((order) => (
-          <View style={styles.tableColumns}>
-            <Text style={{ ...styles.tbody }}>{order.item}</Text>
-            <Text style={{ ...styles.tbody }}>{order.supplier}</Text>
-            <Text style={{ ...styles.tbody }}>{order.quantity}</Text>
-            <RadioButton.Group
-              onValueChange={(newValue) => setValue(newValue)}
-              value={value}
-            >
-              <View
-                style={{
-                  display: "flex",
-                  textAlign: "center",
-                  fontSize: 15,
-                  fontWeight: 500,
-
-                  flexDirection: "row",
-                }}
-              >
-                <RadioButton value={true} />
-                <Text>Complete</Text>
-              </View>
-              <View
-                style={{
-                  display: "flex",
-                  textAlign: "center",
-                  fontSize: 15,
-                  fontWeight: 500,
-
-                  borderBottomColor: "grey",
-                  borderStyle: "solid",
-                  borderBottomWidth: 1,
-                  flexDirection: "row",
-                }}
-              >
-                <RadioButton value={false} />
-                <Text>Incomplete</Text>
-              </View>
-            </RadioButton.Group>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.tableColumns}>
+          <Text style={{ ...styles.thead }}>Name</Text>
+          <Text style={{ ...styles.thead }}>Supplier</Text>
+          <Text style={{ ...styles.thead }}>Quantity</Text>
+          <Text style={{ ...styles.thead }}>Complete status</Text>
+        </View>
+        <ScrollView scrollEnabled={true} style={{ height: 300 }}>
+          {itemData.orderList.map((order, index) => (
+            <TableRow key={index} order={order} />
+          ))}
+        </ScrollView>
+        <View style={styles.infoSection}>
+          <View style={styles.singleRow}>
+            <Text style={styles.label}>Place date:</Text>
+            <Text style={styles.data}>{"2023/10/10"}</Text>
           </View>
-        ))}
-      </ScrollView>
-      <View>
-        <Text>Add review:</Text>
-        <TextInput></TextInput>
-      </View>
-    </SafeAreaView>
+          <View style={styles.singleRow}>
+            <Text style={styles.label}>Order required:</Text>
+            <Text style={styles.data}>{"2023/10/20"}</Text>
+          </View>
+          <View style={styles.singleRow}>
+            <Text style={styles.label}>Site name:</Text>
+            <Text style={styles.data}>{"Malabe"} </Text>
+          </View>
+        </View>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity>
+            <Button
+              textColor="#426252"
+              compact={true}
+              mode="outlined"
+              uppercase
+              dark={true}
+              style={{ paddingLeft: 30, paddingRight: 30 }}
+              onPress={() => setVisible(true)}
+            >
+              Add note
+            </Button>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Button
+              textColor="white"
+              compact={true}
+              mode="elevated"
+              buttonColor="#426252"
+              uppercase
+              style={{ paddingLeft: 30, paddingRight: 30 }}
+            >
+              Save
+            </Button>
+          </TouchableOpacity>
+        </View>
+        <Portal>
+          <Modal
+            visible={visible}
+            onDismiss={() => setVisible(false)}
+            contentContainerStyle={styles.modal}
+          >
+            <TextInput
+              mode="outlined"
+              label="add review"
+              onChangeText={(e) => setReview(e)}
+            />
+            <Text>{review}</Text>
+          </Modal>
+        </Portal>
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
 
@@ -87,11 +112,11 @@ const styles = StyleSheet.create({
   thead: {
     width: "25%",
     display: "flex",
-    textAlign: "center",
+
     fontSize: 15,
     fontWeight: 500,
     backgroundColor: "#FF9933",
-    padding: 10,
+    padding: 6,
     color: "white",
   },
   tbody: {
@@ -105,5 +130,38 @@ const styles = StyleSheet.create({
     borderStyle: "solid",
     borderBottomWidth: 1,
     backgroundColor: "#e3e3e3",
+  },
+  modal: { backgroundColor: "white", padding: 20 },
+  infoSection: {
+    marginTop: 20,
+    width: "100%",
+  },
+  singleRow: {
+    display: "flex",
+    flexDirection: "row",
+    marginBottom: 10,
+  },
+
+  label: {
+    width: "50%",
+    fontSize: 20,
+  },
+  data: {
+    width: "50%",
+    fontSize: 20,
+  },
+  addNoteBtn: {
+    color: "white",
+    width: "fit-content",
+    display: "flex",
+    textAlign: "center",
+    flexDirection: "row",
+  },
+  btnContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    marginTop: 20,
   },
 });
